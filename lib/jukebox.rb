b79cc7c24164a8a9a8051cc47c0203349d0a17e1
Sanam Malik <sanam.malik04@gmail.com>
2020-07-10 01:58:22 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,6 +22,14 @@ def play(songs)
   puts "Please enter a song name or number:"
   songChoice = gets.strip
   songChoice = songChoice.to_i
+  if number < 0 && songs.include?(input)
+    index = songs.index(input)
+    puts "Playing #{songs[index]}"
+  elsif number >= 0 && songs[number] != nil
+    puts "Playing #{songs[number]}"
+  else
+    puts "Invalid input, please try again"
+  end
   puts "Playing #{songs[songChoice-1]}."
 end
 
